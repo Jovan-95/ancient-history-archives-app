dcee0d4738fb7167f4aecfc999aba84db381bdf5
jovan <vuksanovicjovan@hotmail.com>
2025-04-14 19:16:30 +0200

    test fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 
 function Home() {
   const loggedUser = useSelector((state) => state.auth.loggedInUsers);
-  console.log("Currently logged user:", loggedUser);
+  console.log("Currently logged user test:", loggedUser);
   return (
     <div>
       <h1>HOME</h1>
